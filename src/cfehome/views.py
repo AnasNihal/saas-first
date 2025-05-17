a644400cf686ee022e6d09af0010bd26e61f4c6d
AnasNihal <anasnihal03@gmail.com>
2025-05-18 00:06:16 +0530

    first stage stuff

---
+++ src/cfehome/views.py
@@ -0,0 +1,16 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    """
+    Render the home page.
+    """
+    return render(request, 'home.html')
+def about(request):
+    """
+    Render the about page.
+    """
+    return render(request, 'about.html')
+
