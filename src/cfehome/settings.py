d91dae9d4d93bebed4bd7956a6703ae77b3d9c14
AnasNihal <anasnihal03@gmail.com>
2025-05-26 13:10:00 +0530

    Dynamic production ready and deployed

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*@wyspedtq%tuf2ubcr#5)r-_st2s_n=yk6=nvxj=1a!71@lb8'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG',cast=bool)
+
+print(f"DEBUG",DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".onrender.com",# https://saas.prod.onrender.com/
